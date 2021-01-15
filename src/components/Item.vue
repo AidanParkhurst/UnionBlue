357c9e04e484eabd8fe4e96c023148fa08c2a436
Aidan Parkhurst <aidanparkhurst@gmail.com>
2021-01-14 22:14:33 -0500

    Created project, added sample listings

---
+++ src/components/Item.vue
@@ -0,0 +1,25 @@
+<template lang="pug">
+div.inline-block.bg-white.rounded-xl.h-52.shadow-md.border
+  h1.p-4.text-xl.font-sans {{ name }}
+  h2.pl-2 {{ desc }}
+</template>
+
+<script lang="ts">
+export default {
+  name: 'Item',
+  props: {
+    name: {
+      type: String,
+      required: true
+    },
+    desc: {
+      type: String,
+      required: true,
+    },
+    contact: {
+      type: String,
+      required: true,
+    },
+  },
+}
+</script>
