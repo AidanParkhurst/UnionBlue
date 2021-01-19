734af74e6413112d5576dd5e03a37721e62b7308
Aidan Parkhurst <aidanparkhurst@gmail.com>
2021-01-19 01:11:41 -0500

    Read items from file, new file listing, item img

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,32 @@
 <template lang="pug">
 Header
-Item(
-  name='Latex Free Gloves'
-  desc='Non-sterile latex free gloves, coming in boxes of 100'
-  contact='jimmywilbur@mail.gov')
-Item(
-  name='Hand Sanitizer'
-  desc='Gallon jugs of hand sanitizer, comes in boxes of 10'
-  contact='someone@somewhere.edu')
-Item(
-  name='Face Masks'
-  desc='Protective coverings for the mouth and nose, comes in boxes of 100'
-  contact='bobertroberts@dese.net')
+template(v-for='item in items')
+  Item(
+    :name="item.name"
+    :desc="item.desc"
+    :contact="item.contact"
+    :imgsrc="item.img"
+  )
+  hr(class='w-1/2 m-auto')
 </template>
 
 <script lang="ts">
 import Header from './components/Header.vue'
 import Item from './components/Item.vue'
 
+//TODO: Remove this, fetch from backend
+import * as inventory from './assets/ItemsTemp.json'
+
 export default {
   name: 'App',
   components: {
     Header,
     Item
+  },
+  data() {
+    return {
+      items: inventory.items
+    }
   }
 }
 </script>
--- src/components/Item.vue
+++ src/components/Item.vue
@@ -1,9 +1,11 @@
 <template lang="pug">
-div.h-80.w-80.mx-auto.my-20.bg-white.rounded-lg.shadow-md.border
-  div.w-full.h-16.bg-blue-700.text-white
-    h1.p-4.text-3xl.font-sans {{ name }}
-  h1.p-4.text-xl {{ desc }}
-  h1.p-4.text-lg {{ contact }}
+div(class="w-1/2 space-x-64 mx-auto my-12")
+  img(:src='imgsrc' class="inline h-80")
+  div.rounded-lg.shadow-md.border.bg-white(class="inline-block h-80 w-80")
+    div.bg-blue-700.text-white(class="w-full h-16")
+      h1.text-3xl.font-sans.p-4 {{ name }}
+    h1.text-xl.p-4 {{ desc }}
+    h1.text-lg.p-4 {{ contact }}
 
 </template>
 
@@ -20,6 +22,10 @@ export default {
       required: true,
     },
     contact: {
+      type: String,
+      required: true,
+    },
+    imgsrc: {
       type: String,
       required: true,
     },
