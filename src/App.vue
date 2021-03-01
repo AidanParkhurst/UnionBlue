c2ad7a4fcd289457db40a6d3ef7360fef41d3b19
Aidan Parkhurst <aidanparkhurst@gmail.com>
2021-02-28 23:48:33 -0500

    Added / Configured vue router, established home

--- src/App.vue
+++ src/App.vue
@@ -1,40 +1,14 @@
 <template lang="pug">
 Header
-template(v-for='item in items')
-  Item(
-    :name="item.name"
-    :desc="item.desc"
-    :contact="item.contact"
-    :imgsrc="'/img/' + item.img"
-  )
-  hr.border.border-blue-200(class='w-3/5 m-auto')
-Contact(v-if="currentItem")
+router-view
 </template>
 
 <script lang="ts">
 import Header from './components/Header.vue'
-import Item from './components/Item.vue'
-import Contact from './components/Contact.vue'
-
-import {currentItem, getItems} from './composables/item'
-
 export default {
   name: 'App',
   components: {
-    Header,
-    Item,
-    Contact
-  },
-  setup() {
-    let items = getItems()
-    return {
-      items
-    }
-  },
-  data() {
-    return {
-      currentItem: currentItem
-    }
+    Header
   }
 }
 </script>
---
+++ src/views/Home.vue
@@ -0,0 +1,39 @@
+<template lang="pug">
+template(v-for='item in items')
+  Item(
+    :name="item.name"
+    :desc="item.desc"
+    :contact="item.contact"
+    :imgsrc="'/img/' + item.img"
+  )
+  hr.border.border-blue-200(class='w-3/5 m-auto')
+Contact(v-if="currentItem")
+</template>
+
+<script lang="ts">
+import Header from '../components/Header.vue'
+import Item from '../components/Item.vue'
+import Contact from '../components/Contact.vue'
+
+import {currentItem, getItems} from '../composables/item'
+
+export default {
+  name: 'Home',
+  components: {
+    Header,
+    Item,
+    Contact
+  },
+  setup() {
+    let items = getItems()
+    return {
+      items
+    }
+  },
+  data() {
+    return {
+      currentItem: currentItem
+    }
+  }
+}
+</script>
