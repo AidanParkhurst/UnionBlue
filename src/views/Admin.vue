a39e4969a8ef05825d5098a51d23904ff41be1a8
Marcsa1 <doubl3omarc@gmail.com>
2021-03-17 18:30:58 -0400

    added admin to vue-router, item display to admin

---
+++ src/views/Admin.vue
@@ -0,0 +1,42 @@
+<template lang="pug">
+h1.text-center Items:
+div(class="w-1/2 m-auto")
+  table.w-full.text-center.rounded-lg.ring-2.ring-gray-200
+    tr
+      th Name
+      th Description
+      th Actions
+    tr(v-for='item in items')
+      td {{item.name}}
+      td {{item.desc}}
+      td
+        delete-forever.icon(@click='')
+        pencil.icon.ml-2(@click='')
+</template>
+
+<script lang="ts">
+import {getItems} from '@/composables/item'
+import {DeleteForever, Pencil} from 'mdue'
+
+export default({
+  name: "Admin",
+  components: {
+    DeleteForever,
+    Pencil
+  },
+  setup() {
+    let items = getItems()
+    return {
+      items
+    }
+  }
+})
+</script>
+
+<style lang="sass" scoped>
+tr
+  @apply border-b
+
+.icon
+  @apply inline text-gray-400 text-2xl cursor-pointer
+</style>
